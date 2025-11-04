5f9519008e054bd5bc9cd74b11e56144802b688f
nilesbanu <nilesbanu@intheraconsultinggroup.com>
2025-11-04 19:25:58 +0530

    ci test message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           GitHub Action + SonarCloud CI Test Successful!
         </p>
         <a
           className="App-link"
