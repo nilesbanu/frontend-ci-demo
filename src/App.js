a69a140ff702ae0206f7c8cc71303a0acc8beb03
nilesbanu <nilesbanu@intheraconsultinggroup.com>
2025-11-04 23:03:26 +0530

    ci test and sonarcloud

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-           GitHub Action + SonarCloud CI Test Successful!
-        </p>
+        <p>GitHub Action + SonarCloud CI Test Successful!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
